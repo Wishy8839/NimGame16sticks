3e289433bc59b023a88babb2ff3d31104430925d
Wishy8839 <121278607+Wishy8839@users.noreply.github.com>
2023-03-18 16:50:25 -0400

    updated main.py
    The check actual now works & you can't use negatives or use Zero
    Made the program a bit smarter so that it'll be able to win most of the time

--- main.py
+++ main.py
@@ -8,31 +8,48 @@ print("The person to pick the last stick loses")
 print("----------------------------------------")
 
 
-while sticks != 1:
+while sticks != 1 or -1 or -2 or -3 :
   Ans = int(input("how many sticks do you wanna use (1, 2, or 3): "))
-  while sticks == 3:
-    while Ans > 2:
-      print("You can't take all the sticks")
-      Ans = int(input("how many sticks do you wanna use (1 or 2): "))
-  while sticks == 2:
-    while Ans > 1:
-      print("You can't take all of the sticks")
-      Ans = int(input("how many sticks do you wanna use (1):  "))
-  while Ans > 3:
-    print("thats more than 3, try again")
-    Ans = int(input("how many sticks do you wanna use (1, 2, or 3):  "))
-    if sticks == 3:
+  if sticks == 3:
       while Ans > 2:
         print("You can't take all of the sticks")
         Ans = int(input("Pick a number (1 or 2)"))
-    while sticks == 2:
-      while Ans > 1:
+        if Ans == 2:
+          compwin = True
+      while sticks == 32:
+       while Ans > 1:
         print("You can't take all the sticks")
         Ans == int(input("Pick a number (1)"))
+        if Ans == 1:
+          compwin = True
+  while Ans <= -1:
+    print("You can't take away a negative!")
+    Ans = int(input("How many sticks do you wanna use (1, 2, or 3:  )"))
+  while Ans == 0:
+    print("you can't skip your turn!")
+    Ans = int(input("how many sticks do you wanna use (1, 2, or 3:  )"))
+  while Ans > 3:
+    print("thats more than 3, try again")
+    Ans = int(input("how many sticks do you wanna use (1, 2, or 3):  "))
   sticks = sticks - Ans
   print(f"sticks left: {sticks}")
   
   CompAns = random.randint(1,3)
+  if sticks == 12:
+    if CompAns == 1 or 2:
+      CompAns = 3
+  if sticks == 10:
+    if CompAns ==  2 or 3:
+      CompAns = 1
+  if sticks == 8:
+    if CompAns == 1 or 2:
+      CompAns == 3
+  if sticks == 7:
+    if CompAns == 1 or 3:
+      CompAns = 2
+  if sticks == 6:
+    if CompAns == 2 or 3:
+      CompAns = 1
   if sticks == 4:
     if CompAns == 1 or 2 or 3:
       CompAns = 3
