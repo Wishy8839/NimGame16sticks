3a6ee91943c59c0b89d7ecff9a89ab5cf44a0082
Wishy8839 <121278607+Wishy8839@users.noreply.github.com>
2023-03-18 14:31:53 -0400

    Update main.py
    updated checks so it now checks if the user takes all of the sticks or not & even made sure if there's 2 sticks you have to pick up 1 to win

--- main.py
+++ main.py
@@ -9,14 +9,34 @@ print("----------------------------------------")
 
 
 while sticks != 1:
-  Ans = int(input("how many sticks do you wanna use (1, 2, or 3):  "))
+  Ans = int(input("how many sticks do you wanna use (1, 2, or 3): "))
+  while sticks == 3:
+    while Ans > 2:
+      print("You can't take all the sticks")
+      Ans = int(input("how many sticks do you wanna use (1 or 2): "))
+  while sticks == 2:
+    while Ans > 1:
+      print("You can't take all of the sticks")
+      Ans = int(input("how many sticks do you wanna use (1):  "))
   while Ans > 3:
     print("thats more than 3, try again")
     Ans = int(input("how many sticks do you wanna use (1, 2, or 3):  "))
+    if sticks == 3:
+      while Ans > 2:
+        print("You can't take all of the sticks")
+        Ans = int(input("Pick a number (1 or 2)"))
+    while sticks == 2:
+      while Ans > 1:
+        print("You can't take all the sticks")
+        Ans == int(input("Pick a number (1)"))
   sticks = sticks - Ans
   print(f"sticks left: {sticks}")
   
   CompAns = random.randint(1,3)
+  if sticks == 4:
+    if CompAns == 1 or 2 or 3:
+      CompAns = 3
+      compwin = True
   if sticks == 3:
     if CompAns == 1 or 2 or 3:
       CompAns = 2
@@ -31,5 +51,10 @@ while sticks != 1:
     print(f"sticks left: {sticks}")
   if compwin == True:
     print("The computer has won!")
+    sticks = 16
+    compwin = False
+    print("you'll play again now")
   elif sticks == 1:
     print("The computer has lost")
+    sticks = 16
+    print("you'll play again now")
